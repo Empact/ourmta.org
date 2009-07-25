d3c3612f30595cb4613abdd50fa4417d2bef3af8
Michael Haddad <michael@ludditetechnology.com>
2009-07-25 18:12:54 -0400

    user can users who have publicly pledged their support

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,3 +1,5 @@
 class Supporter < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+  named_scope :public, {:conditions => {:pledged_public_support => true}}
 end
