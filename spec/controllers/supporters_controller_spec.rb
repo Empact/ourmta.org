f74eeb4af99619db03829d9b01b37e67ea7d6eb9
Ben Woosley <ben.woosley@gmail.com>
2009-07-26 10:15:06 -0400

    Refactor supporters controller spec a bit

--- spec/controllers/supporters_controller_spec.rb
+++ spec/controllers/supporters_controller_spec.rb
@@ -11,18 +11,30 @@ describe SupportersController do
   end
 
   describe "POST create" do
+    def do_post(supporter_attrs = {})
+      post :create, {:supporter => supporter_attrs.reverse_merge(:email => Faker::Internet.email)}
+    end
+
     it "should create a new supporter" do
-      lambda { 
-        post :create, {:supporter => {:email => Faker::Internet.email}}
+      lambda {
+        do_post
       }.should change(Supporter, :count).by(1)
+    end
+
+    it "should redirect to home" do
+      do_post
       response.should redirect_to(:controller => "home", :action => "index")
     end
 
-    it "should not create a supporter who has pledged public support but has not provided a name" do
-      lambda {
-        post :create, {:supporter => {:email => Faker::Internet.email, :pledged_public_support => true}}
-      }.should_not change(Supporter, :count)
-      response.body.should include("Name must be present if you are pledging public support")       
+    context "when the user has pledged public support" do
+      context "but has not provided a name" do
+        it "should not create a supporter" do
+          lambda {
+            do_post :pledged_public_support => true
+          }.should_not change(Supporter, :count)
+          response.body.should include("Name must be present if you are pledging public support")       
+        end
+      end
     end
   end
 end
