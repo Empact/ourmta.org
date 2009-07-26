69a35e630165d5f7ab6986bc84e36dc817e2fd6d
Ben Woosley <ben.woosley@gmail.com>
2009-07-26 10:16:24 -0400

    Clean up supporter spec

--- spec/models/supporter_spec.rb
+++ spec/models/supporter_spec.rb
@@ -1,22 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Supporter do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    create_supporter.should be_valid
-  end
-
   describe ".public" do
     it "should return users who have publicly pledged their support" do
       create_supporter(:pledged_public_support => false)
-      create_supporter(:pledged_public_support => true, :name => Faker::Name.new)
+      public_supporter = create_supporter(:pledged_public_support => true, :name => Faker::Name.new)
 
-      Supporter.public.count.should == 1
+      Supporter.public.should == [public_supporter]
     end
   end
 end
