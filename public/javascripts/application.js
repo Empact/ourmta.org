edc28aba29c6912a5bce6e967b936bf5666adc81
Michael <michael@michael-desktop.(none)>
2009-07-26 03:02:51 -0400

    public pledge information section is toggled based on checkbox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function () {
+    $('#supporter_pledged_public_support').click( function() {
+        if($(this).attr('checked')) {
+            $('#public_pledge_information').blindDown();
+        } else {
+            $('#public_pledge_information').blindUp();
+        }
+    });
+});
