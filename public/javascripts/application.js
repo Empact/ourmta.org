bcc6725753042856edfa4c39c68388ba2130fee0
Ben Woosley <ben.woosley@gmail.com>
2009-07-29 19:11:18 -0400

    Change is a more appropriate event for the public support toggle

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#supporter_pledged_public_support').click( function() {
+    $('#supporter_pledged_public_support').change(function() {
         if($(this).attr('checked')) {
             $('#public_pledge_information').blindDown();
         } else {
