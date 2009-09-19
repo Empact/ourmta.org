92356de6d56113e405931420b98e4327eb21674f
Ben Woosley <ben.woosley@gmail.com>
2009-09-19 17:01:43 -0400

    Seperately test for presence of MailingListStorage list_id and API_KEY

--- lib/mailing_list_storage.rb
+++ lib/mailing_list_storage.rb
@@ -2,7 +2,8 @@ class MailingListStorage
   API_KEY = ENV['MAIL_CHIMP_API_KEY']
 
   def initialize(list_id)
-    raise "Invalid Campaign Monitor setup" if list_id.blank? || API_KEY.blank?
+    raise "Invalid mail list storage setup: no list id" if list_id.blank?
+    raise "Invalid mail list storage setup: no api key" if API_KEY.blank?
     @list_id = list_id
     @hominid = Hominid.new(:api_key => API_KEY)
   end
