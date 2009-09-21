b832ae1c99d057b81f8adad0be58fa26114ba860
Ben Woosley <ben.woosley@gmail.com>
2009-09-21 02:54:44 -0400

    Introduce email_address_path helper for consistent email links, and point it at g-mail, since that already works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def email_address_path
+    'mailto:fixthemta@gmail.com'
+  end
 end
