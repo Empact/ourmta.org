ccd799226ea73e27284032c8be2baaa46d0b988a
Ben Woosley <ben.woosley@gmail.com>
2009-09-26 18:23:41 -0400

    Turn on caching, primarily to prevent us from hitting meetup's limits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  expires_in 2.hours, :public => true
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
