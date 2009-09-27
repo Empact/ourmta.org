b3c01740603462961fa27fa235838e9090557a49
Ben Woosley <ben.woosley@gmail.com>
2009-09-26 20:22:30 -0400

    Replace all incidences of FixMta.org with FixMTA

--- spec/controllers/information_controller_spec.rb
+++ spec/controllers/information_controller_spec.rb
@@ -10,7 +10,7 @@ describe InformationController do
 
     it "should return the home page" do
       do_get
-      response.body.should include("Welcome to FixMta.org")
+      response.body.should include("Welcome to FixMTA")
     end
 
     it_should_behave_like "has sign-up screen"
--- spec/support/shared_specs/has_signup_form.rb
+++ spec/support/shared_specs/has_signup_form.rb
@@ -1,6 +1,6 @@
 describe "has sign-up screen", :shared => true do
   it "should provide a sign-up screen" do
     do_get
-    response.body.should include("Sign up to FixMta.org")
+    response.body.should include("Sign up to FixMTA")
   end
 end
