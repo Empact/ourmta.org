98e9f309b92caf389214b318b401c8d65403603a
Ben Woosley <ben.woosley@gmail.com>
2009-10-01 00:01:47 -0400

    Add BlogPost tumblr-fetching model

---
+++ app/models/blog_post.rb
@@ -0,0 +1,27 @@
+class BlogPost
+  class << self
+    def first
+      new(RestClient.get('http://blog.ourmta.org/api/read/json?num=1&type=text'))
+    end
+  end
+
+  def initialize(json)
+    @attrs = JSON.parse(json.sub('var tumblr_api_read = ', '').chop)['posts'][0]
+  end
+  
+  def url
+    @attrs['url-with-slug']
+  end
+  
+  def posted_on
+    DateTime.parse(@attrs['date-gmt']).to_date
+  end
+  
+  def title
+    @attrs['regular-title']
+  end
+  
+  def body
+    @attrs['regular-body']
+  end
+end
